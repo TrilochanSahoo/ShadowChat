bd262dafb9c21eff3d28d9177f8e2b73187a9d79
TrilochanSahoo <trilochansahoo026@gmail.com>
2025-04-12 15:42:08 +0530

    setup project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Landing } from './components/Landing'
+import { Room } from './components/Room'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing/>} ></Route>
+      <Route path="/room" element={<Room/>} ></Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Room.tsx
@@ -0,0 +1,7 @@
+export const Room = ()=>{
+    return (
+        <>
+        Room
+        </>
+    )
+}
